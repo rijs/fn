d8129c6fb736ee0973e9e71c355caf9a29853795
pemrouz <p3pedram@hotmail.com>
2015-10-16 01:06:34 +0100

    build dist

--- dist/client.js
+++ dist/client.js
@@ -33,101 +33,22 @@ var is = _interopRequire(require("utilise/is"));
 var fn = _interopRequire(require("utilise/fn"));
 
 log = log("[ri/types/fn]");
-},{"utilise/fn":2,"utilise/includes":3,"utilise/is":4,"utilise/log":5}],2:[function(require,module,exports){
-module.exports = require('fn')
-},{"fn":6}],3:[function(require,module,exports){
-module.exports = require('includes')
-},{"includes":8}],4:[function(require,module,exports){
-module.exports = require('is')
-},{"is":9}],5:[function(require,module,exports){
-module.exports = require('log')
-},{"log":10}],6:[function(require,module,exports){
-var is = require('is')
+},{"utilise/fn":3,"utilise/includes":4,"utilise/is":5,"utilise/log":6}],2:[function(require,module,exports){
+module.exports = typeof window != 'undefined'
+},{}],3:[function(require,module,exports){
+var is = require('utilise/is')
 
 module.exports = function fn(candid){
   return is.fn(candid) ? candid
        : (new Function("return " + candid))()
 }
-},{"is":7}],7:[function(require,module,exports){
-module.exports = is
-is.fn     = isFunction
-is.str    = isString
-is.num    = isNumber
-is.obj    = isObject
-is.lit    = isLiteral
-is.bol    = isBoolean
-is.truthy = isTruthy
-is.falsy  = isFalsy
-is.arr    = isArray
-is.null   = isNull
-is.def    = isDef
-is.in     = isIn
-
-function is(v){
-  return function(d){
-    return d == v
-  }
-}
-
-function isFunction(d) {
-  return typeof d == 'function'
-}
-
-function isBoolean(d) {
-  return typeof d == 'boolean'
-}
-
-function isString(d) {
-  return typeof d == 'string'
-}
-
-function isNumber(d) {
-  return typeof d == 'number'
-}
-
-function isObject(d) {
-  return typeof d == 'object'
-}
-
-function isLiteral(d) {
-  return typeof d == 'object' 
-      && !(d instanceof Array)
-}
-
-function isTruthy(d) {
-  return !!d == true
-}
-
-function isFalsy(d) {
-  return !!d == false
-}
-
-function isArray(d) {
-  return d instanceof Array
-}
-
-function isNull(d) {
-  return d === null
-}
-
-function isDef(d) {
-  return typeof d !== 'undefined'
-}
-
-function isIn(set) {
-  return function(d){
-    return  set.indexOf 
-         ? ~set.indexOf(d)
-         :  d in set
-  }
-}
-},{}],8:[function(require,module,exports){
+},{"utilise/is":5}],4:[function(require,module,exports){
 module.exports = function includes(pattern){
   return function(d){
-    return ~d.indexOf(pattern)
+    return d && d.indexOf && ~d.indexOf(pattern)
   }
 }
-},{}],9:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 module.exports = is
 is.fn     = isFunction
 is.str    = isString
@@ -195,15 +116,15 @@ function isDef(d) {
 
 function isIn(set) {
   return function(d){
-    return  set.indexOf 
-         ? ~set.indexOf(d)
-         :  d in set
+    return !set ? false  
+         : set.indexOf ? ~set.indexOf(d)
+         : d in set
   }
 }
-},{}],10:[function(require,module,exports){
-var is = require('is')
-  , to = require('to')
-  , owner = require('owner')
+},{}],6:[function(require,module,exports){
+var is = require('utilise/is')
+  , to = require('utilise/to')
+  , owner = require('utilise/owner')
 
 module.exports = function log(prefix){
   return function(d){
@@ -214,20 +135,32 @@ module.exports = function log(prefix){
     return console.log.apply(console, args), d
   }
 }
-},{"is":11,"owner":12,"to":14}],11:[function(require,module,exports){
-arguments[4][9][0].apply(exports,arguments)
-},{"dup":9}],12:[function(require,module,exports){
+},{"utilise/is":5,"utilise/owner":7,"utilise/to":8}],7:[function(require,module,exports){
 (function (global){
-module.exports = require('client') ? /* istanbul ignore next */ window : global
+module.exports = require('utilise/client') ? /* istanbul ignore next */ window : global
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"client":13}],13:[function(require,module,exports){
-module.exports = typeof window != 'undefined'
-},{}],14:[function(require,module,exports){
+},{"utilise/client":2}],8:[function(require,module,exports){
 module.exports = { 
-  arr : toArray
+  arr: toArray
+, obj: toObject
 }
 
 function toArray(d){
   return Array.prototype.slice.call(d, 0)
+}
+
+function toObject(d) {
+  var by = 'id'
+    , o = {}
+
+  return arguments.length == 1 
+    ? (by = d, reduce)
+    : reduce.apply(this, arguments)
+
+  function reduce(p,v,i){
+    if (i === 0) p = {}
+    p[v[by]] = v
+    return p
+  }
 }
 },{}]},{},[1]);
